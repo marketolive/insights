8daa5d96dfee702cdff93cbd319216f2f97e362e
Brian Fisher <brian.charles.fisher@gmail.com>
2018-03-01 10:44:03 -0500

    Testing Session Changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, session
+from flask_session import Session
 
 app = Flask(__name__)
+app.secret_key = '123123'
+app.config['SESSION_TYPE'] = 'filesystem'
 app.config.from_object('config')
+Session(app)
 
 if app.config['FORCE_SSL'] and not app.debug:
   from flask_sslify import SSLify
