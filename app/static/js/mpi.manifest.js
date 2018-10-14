b32000117f74fe802bca98d6d8d321af8196038a
Hunter Grant <huntergrant@gmail.com>
2018-10-14 00:52:33 -0400

    Q3 release updates

--- app/static/js/mpi.manifest.js
+++ app/static/js/mpi.manifest.js
@@ -1,4 +1,4 @@
-! function(e) {
+! function (e) {
     function n(r) {
         if (t[r]) return t[r].exports;
         var o = t[r] = {
@@ -9,60 +9,60 @@
         return e[r].call(o.exports, o, o.exports, n), o.l = !0, o.exports
     }
     var r = window.webpackJsonp;
-    window.webpackJsonp = function(t, a, c) {
+    window.webpackJsonp = function (t, c, a) {
         for (var u, i, f, p = 0, l = []; p < t.length; p++) i = t[p], o[i] && l.push(o[i][0]), o[i] = 0;
-        for (u in a) Object.prototype.hasOwnProperty.call(a, u) && (e[u] = a[u]);
-        for (r && r(t, a, c); l.length;) l.shift()();
-        if (c)
-            for (p = 0; p < c.length; p++) f = n(n.s = c[p]);
+        for (u in c) Object.prototype.hasOwnProperty.call(c, u) && (e[u] = c[u]);
+        for (r && r(t, c, a); l.length;) l.shift()();
+        if (a)
+            for (p = 0; p < a.length; p++) f = n(n.s = a[p]);
         return f
     };
     var t = {},
         o = {
             2: 0
         };
-    n.e = function(e) {
+    n.e = function (e) {
         function r() {
             u.onerror = u.onload = null, clearTimeout(i);
             var n = o[e];
             0 !== n && (n && n[1](new Error("Loading chunk " + e + " failed.")), o[e] = void 0)
         }
         var t = o[e];
-        if (0 === t) return new Promise(function(e) {
+        if (0 === t) return new Promise(function (e) {
             e()
         });
         if (t) return t[2];
-        var a = new Promise(function(n, r) {
+        var c = new Promise(function (n, r) {
             t = o[e] = [n, r]
         });
-        t[2] = a;
-        var c = document.getElementsByTagName("head")[0],
+        t[2] = c;
+        var a = document.getElementsByTagName("head")[0],
             u = document.createElement("script");
         u.type = "text/javascript", u.charset = "utf-8", u.async = !0, u.timeout = 12e4, n.nc && u.setAttribute("nonce", n.nc), u.src = n.p + "" + ({
             0: "app",
             1: "vendor"
         }[e] || e) + "." + {
-            0: "050a2921ffd289e0b075",
-            1: "105d31baefb54cb1de86"
+            0: "e886b4e0cae9e09ffd65",
+            1: "0457e75b7d6417f4437c"
         }[e] + ".js";
         var i = setTimeout(r, 12e4);
-        return u.onerror = u.onload = r, c.appendChild(u), a
-    }, n.m = e, n.c = t, n.d = function(e, r, t) {
+        return u.onerror = u.onload = r, a.appendChild(u), c
+    }, n.m = e, n.c = t, n.d = function (e, r, t) {
         n.o(e, r) || Object.defineProperty(e, r, {
             configurable: !1,
             enumerable: !0,
             get: t
         })
-    }, n.n = function(e) {
-        var r = e && e.__esModule ? function() {
+    }, n.n = function (e) {
+        var r = e && e.__esModule ? function () {
             return e.default
-        } : function() {
+        } : function () {
             return e
         };
         return n.d(r, "a", r), r
-    }, n.o = function(e, n) {
+    }, n.o = function (e, n) {
         return Object.prototype.hasOwnProperty.call(e, n)
-    }, n.p = "", n.oe = function(e) {
+    }, n.p = "", n.oe = function (e) {
         throw console.error(e), e
     }
 }([]);
